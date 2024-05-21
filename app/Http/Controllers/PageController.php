3251e8dc283d66aaad22714795c916ee935cfbfe
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-21 17:27:50 +0200

    feat add complete project and bonus

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index (){
+        $movies = Movie::all();
+        // dd($movies);
+        return view('home',['movies' => $movies]);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>come stai</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="card m-2 border border-primary" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title bg-primary-subtle ">Title: {{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{ $movie->original_title }}</h6>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Date: {{ $movie->date }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
